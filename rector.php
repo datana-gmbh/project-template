1332bf7bc147873c840aaaa38ceab51f59c72e46
Oskar Stark <oskarstark@googlemail.com>
2023-11-14 10:44:33 +0100

    Enhancement: Require `rector/rector` with version `^0.18.7`
    _Created by [PR-Factory](https://github.com/datana-gmbh/pr-factory)_ using the following command:
    
    ```bash
    
    symfony console \
       project:composer:require:rector-0.18.7 \
       --delete-branch-if-exists \
       -vv \
       all
    
    ```

--- rector.php
+++ rector.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
 use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
